be2d367ad8fca8c52e75d2f3d0d9c2ae9cf8b113
79612 <79612@DESKTOP-O1P5TC5>
2024-02-22 10:34:22 +0300

    Добавьте файлы проекта.

---
+++ циклы/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace циклы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userRepeat;
+
+            Console.Write("введите количество повторений: ");
+            userRepeat = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < userRepeat; i++)
+            {
+                Console.WriteLine("повторяется " + (userRepeat) + " раз.");
+            }
+        }
+    }
+}
